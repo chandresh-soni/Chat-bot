c25f4f6dba1dd11b3a49966d011883724d6da4c5
Chandresh Soni <chandresh2702@gmail.com>
2023-09-23 18:11:31 +0530

    Uploading project

---
+++ dialogue_manager.py
@@ -0,0 +1,142 @@
+import os
+from sklearn.metrics.pairwise import pairwise_distances , pairwise_distances_argmin
+from intent_Main import *
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from utils import *
+import college_retrieval
+import programming_retrieval 
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from keras.models import load_model
+import numpy as np
+from emo_utils import *
+import random
+import emojifyer
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.CRITICAL)
+
+
+#add data.txt if needed in filenames
+model = load_model('data/my_model.h5')
+
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+filenames=["data.txt"]
+files={}       
+for filename in filenames:     
+    with open("data/"+filename, 'r', encoding='utf-8') as f:
+        files[filename]= f.read().split('\n')
+        files[filename] = [re.sub(r"\[\w+\]",'hi',line) for line in files[filename]]
+        files[filename]= [" ".join(re.findall(r"\w+",line)) for line in files[filename]]
+
+class ThreadRanker(object):
+    def __init__(self, paths):
+        self.word_embeddings, self.embeddings_dim = load_embeddings(paths['WORD_EMBEDDINGS'])
+        self.thread_embeddings_folder = paths['THREAD_EMBEDDINGS_FOLDER']
+        self.programming=programming_retrieval.Retrieval('programming')
+
+    def __load_embeddings_by_tag(self, tag_name):
+        embeddings_path = os.path.join(self.thread_embeddings_folder, tag_name + ".pkl")
+        thread_ids, thread_embeddings = unpickle_file(embeddings_path)
+        return thread_ids, thread_embeddings
+
+    def get_best_thread(self, question, tag_name):
+
+        #Returns id of the most similar thread for the question.
+        #The search is performed across the threads with a given tag.
+
+        thread_ids, thread_embeddings = self.__load_embeddings_by_tag(tag_name)
+        question_vec = question_to_vec(question, self.word_embeddings, self.embeddings_dim)
+        best_thread = pairwise_distances_argmin(
+            X=question_vec.reshape(1, self.embeddings_dim),
+            Y=thread_embeddings,
+            metric='cosine'
+        )
+        best_thread_similarity = np.min(pairwise_distances(
+            X=question_vec.reshape(1, self.embeddings_dim),
+            Y=thread_embeddings,
+            metric='cosine'
+        ))
+        #print(best_thread_similarity)
+        reply=self.programming.Main(question)
+        if reply != "Please refer kammand prompt discord or ask you mentor for more info :)":
+            return reply
+        else:
+            if best_thread_similarity>=0.45: 
+                return f'I think its about {tag_name}\n This thread might help you: https://stackoverflow.com/questions/{thread_ids[best_thread][0]}'
+            else:
+                return "Please refer to kammand prompt discord or ask for your mentor" 
+class DialogueManager(object):
+    def __init__(self, paths):
+        print("Loading resources...")
+
+        # Intent recognition:
+        self.intent_recognizer = IntentManager(paths)
+        self.tfidf_vectorizer = unpickle_file(paths['TFIDF_VECTORIZER'])
+
+        self.ANSWER_TEMPLATE = 'I think its about %s\n This thread might help you: https://stackoverflow.com/questions/%s'
+
+        # Goal-oriented part:
+        self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
+        self.thread_ranker = ThreadRanker(paths)
+
+        # Chit-chat part
+        self.create_chitchat_bot()
+        self.college=college_retrieval.Retrieval('college')
+        self.programming=programming_retrieval.Retrieval('college')
+    def create_chitchat_bot(self):
+        """Initializes self.chitchat_bot with some conversational model."""
+        
+        self.chatbot = ChatBot("Fresher's Friend")
+        self.chatbot.trainer2=ListTrainer(self.chatbot)
+        self.chatbot.trainer=ChatterBotCorpusTrainer(self.chatbot)
+        self.chatbot.trainer.train("chatterbot.corpus.english.greetings")
+        #self.chatbot.trainer.train("chatterbot.corpus.english.conversations")
+        for filename in filenames:
+            self.chatbot.trainer2.train(files[filename])
+
+
+    def generate_answer(self, question):
+        """Combines stackoverflow and chitchat parts using intent recognition."""
+
+        # Recognize intent of the question using `intent_recognizer`.
+        # Don't forget to prepare question and calculate features for the question.
+        
+        prepared_question = text_prepare(question)
+        features = self.tfidf_vectorizer.transform([prepared_question])
+        intent = self.intent_recognizer.Main(question)
+        #intent='gcs'
+        # Chit-chat part:   
+        if intent == 'dialogue':
+            """
+            # Pass question to chitchat_bot to generate a response.
+            reply=self.college.Main(question)
+            if reply !="Please refer GCS facebook page or ask you mentor for more info :)":
+                return reply
+            else:   
+            """
+            reply=self.college.Main(question)
+            if reply!="Please refer GCS facebook page or ask you mentor for more info :)":
+                return reply
+            else:
+                reply=self.programming.Main(question)
+                if reply!="Please refer kammand prompt discord or ask you mentor for more info :)":
+                    return reply
+                else:
+                    response = str(self.chatbot.get_response(prepared_question))
+                    temp=np.random.choice(2,p=[0.5,0.5])
+                    times=np.random.choice([1,2,3,4],p=[0.5,0.3,0.1,0.1])
+                    if temp==0:
+                        print("EMOJI!!!!!")
+                        response= response + times*(label_to_emoji(emojifyer.predict_emoji(model,response,word_to_index)).strip())
+                    return response
+        elif intent=="mandi":
+            reply=self.college.Main(question)
+            return reply
+        # Goal-oriented part:
+        elif intent=="stackoverflow":
+            tag = self.tag_classifier.predict(features)[0]
+            reply = self.thread_ranker.get_best_thread(prepared_question, tag)
+            return reply
+        
---
+++ main_bot.py
@@ -0,0 +1,128 @@
+import requests
+import time
+import argparse
+import os
+from os import path
+import json
+from utils import *
+from dialogue_manager import DialogueManager
+import spacy
+import random
+from requests.compat import urljoin
+import emoji
+import re
+import voice_recognition
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"
+        u"\U0001F300-\U0001F5FF"
+        u"\U0001F680-\U0001F6FF"
+        u"\U0001F1E0-\U0001F1FF"
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
+
+
+
+with open("data/campus_answers.txt","r",encoding="utf-8") as f:
+        answers= f.read().split('\n')
+class BotHandler(object):
+    """
+        BotHandler is a class which implements all back-end of the bot.
+        It has tree main functions:
+            'get_updates' — checks for new messages
+            'send_message' – posts new message to user
+            'get_answer' — computes the most relevant on a user's question
+    """
+
+    def __init__(self, token, dialogue_manager):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+        self.dialogue_manager = dialogue_manager
+
+    def get_updates(self, offset=None, timeout=30):
+        params = {"timeout": timeout, "offset": offset}
+        raw_resp = requests.get(urljoin(self.api_url, "getUpdates"), params)
+        try:
+            resp = raw_resp.json()
+        except json.decoder.JSONDecodeError as e:
+            print("Failed to parse response {}: {}.".format(raw_resp.content, e))
+            return []
+
+        if "result" not in resp:
+            return []
+        return resp["result"]
+
+    def send_message(self, chat_id, text):
+        params = {"chat_id": chat_id, "text": text}
+        return requests.post(urljoin(self.api_url, "sendMessage"), params)
+
+    def get_answer(self, question):
+        if question == '/start':
+            return "Hi there, How can I help you today?"
+        elif question == "/random":
+            return random.choice(answers)
+        """elif question.lower()=="what is pmc" or question.lower()== "what is pmc?":
+            return "Photography and Moviemaking Club (PMC) is the official photography and videography club of IIT Mandi. It is one of the most prominent and influential clubs of the cultural society. It conducts several events, workshops and sessions all year round. It has brought numerous laurels to the institute. For more info visit https://wiki.iitmandi.co.in/p/Photography_and_Moviemaking_Club"
+        """
+        reply=self.dialogue_manager.generate_answer(question)
+        print(reply)
+        return reply
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', type=str, default='')
+    return parser.parse_args()
+
+
+def is_unicode(text):
+    return len(text) == len(text.encode("utf-8"))
+
+
+def main():
+    token = '1547700302:AAERs9g0D40N-VvW0TIh5jvJc1EujMjC8nc'
+
+    if not token:
+        if not "TELEGRAM_TOKEN" in os.environ:
+            print("Please, set bot token through --token or TELEGRAM_TOKEN env variable")
+            return
+        token = os.environ["TELEGRAM_TOKEN"]
+
+   
+    # This is the point where you plug it into the Telegram bot. 
+    # Do not forget to import all needed dependencies when you do so.
+    
+    dialogue_manager = DialogueManager(RESOURCE_PATH)
+    dialogue_manager.create_chitchat_bot()
+    bot = BotHandler(token, dialogue_manager)
+   
+    print("Ready to talk!")
+    offset = 0
+    while True:
+        updates = bot.get_updates(offset=offset)
+        for update in updates:
+            print("An update received.")
+            if "message" in update:
+                chat_id = update["message"]["chat"]["id"]
+                if "text" in update["message"]:
+                    text = update["message"]["text"]
+                    text=text.encode('ascii', 'ignore').decode('ascii')
+                    if is_unicode(text):
+                        print("Update content: {}".format(update))
+                        bot.send_message(chat_id, bot.get_answer(text))
+                    else:
+                        bot.send_message(chat_id, "Hmm, you are sending some weird characters to me...")
+                if "voice" in update["message"]:
+                    file_id=update["message"]["voice"]["file_id"]
+                    text=voice_recognition.get(update["message"],token)
+                    print(f"Did u said '{text}'")
+                    bot.send_message(chat_id, f"Did u said '{text}'")
+                    print("Update content: {}".format(update))
+                    bot.send_message(chat_id, bot.get_answer(text))
+            
+            offset = max(offset, update['update_id'] + 1)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
+ 
